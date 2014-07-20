a28d7ee32b2e06b9268b24576023fcfd071422a9
Dorrene Brown <dorrene@outlook.com>
2014-07-20 12:11:56 -0700

    Fixed file loading issue, works now

--- rprogramming/proj1/pollutantmean.R
+++ rprogramming/proj1/pollutantmean.R
@@ -1,30 +1,17 @@
 getMonitors <- function(directory, id) {
-  paths <- list()
+  loadedData <- list()
+  
+  #load each desired file (with proper path formatting) into individual dataframes
   for (i in id) {
     path <- paste(directory, "/", sprintf("%03d.csv", i), sep="")
-    
-    temp <- read.csv(path)
-    if (!exists("data")) {
-      data <- temp
-    } else {
-      data <- rbind(temp)
-    }
-    rm(temp)
-  }
+    loadedData[[length(loadedData) + 1L]] <- read.csv(path)
+  }  
   
-  data
+  #rbind all data frames to make analysis easier
+  do.call(rbind, loadedData)
 }
 
 pollutantmean <- function(directory, pollutant, id=1:332) {
   data<-getMonitors(directory, id)
   round(mean(data[,pollutant], na.rm=TRUE), digits=3)
 }
-
-#vars to make calling the above functions easier
-dir <- "C:/Users/Dorrene/code/coursera/rprogramming/proj1/specdata"
-monitors <- 23
-pollutants<- c("nitrate", "sulfate")
-
-#actually get the mean (testing)
-mean <- pollutantmean(dir, pollutants[1], monitors)
-data <- getMonitors(dir, monitors)
